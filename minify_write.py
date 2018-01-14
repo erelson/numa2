6887fd23024b3f607a11e892b1fce5f4757da3c2
Eric Relson <gertlex@gmail.com>
2018-01-14 11:26:16 -0800

    Improve simple minification

--- minify_write.py
+++ minify_write.py
@@ -6,7 +6,7 @@ except ImportError:
     HAVE_MNFY = False
 
 
-def write_minified(infile, outfile, minify=True):
+def write_minified(infile, outfile, minify=True, readable=False):
     
     print("Turning {0} into {1}...".format(infile, outfile))
 
@@ -17,6 +17,14 @@ def write_minified(infile, outfile, minify=True):
                 source = source_file.read()
             with open (outfile, 'w') as fw:
                 fw.write(source)
+        elif minify and readable:
+            print("Using simple readable minification")
+            with open(infile, 'r') as fr, open(outfile, 'w') as fw:
+                for line in fr:
+                    # Check for empty lines or comment lines, and skip them
+                    if not line.lstrip() or line.lstrip()[0] == '#':
+                        continue
+                    fw.write(line)
         # Use mnfy lib to minify
         elif HAVE_MNFY:
             print("Using mnfy library!")
@@ -33,14 +41,30 @@ def write_minified(infile, outfile, minify=True):
             minifier.visit(source_ast)
             with open (outfile, 'w') as fw:
                 fw.write(str(minifier))
-        # Backup simple removal of blank lines and comments
+        # More aggressive simple removal of blank lines and comments
         else:
             print("Using simple minification")
             with open(infile, 'r') as fr, open(outfile, 'w') as fw:
+                commabuffer = ""
                 for line in fr:
                     # Check for empty lines or comment lines, and skip them
                     if not line.lstrip() or line.lstrip()[0] == '#':
                         continue
-                    fw.write(line)
+
+                    if commabuffer:
+                        commabuffer += line.strip()
+                        #if commabuffer[-1] != ",":
+                        if commabuffer[-1] not in "[(,":
+                            fw.write(commabuffer + "\n")
+                            commabuffer = ""
+                    #elif line.rstrip()[-1] == ",":
+                    elif line.rstrip()[-1] in "[(,":
+                        commabuffer = line.rstrip()
+                    else:
+                        fw.write(line)
+                        commabuffer = ""
+                if commabuffer:
+                    fw.write(commabuffer)
+
     except SyntaxError as e:
         print("ERROR: Syntax error when minifying {0}".format(infile))
