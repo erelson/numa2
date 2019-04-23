f07a9632eee1181886de9168bdb6826f0b4afdc4
Eric Relson <erelson@fetchrobotics.com>
2019-04-22 23:04:35 -0700

    Fix error in speedPhaseFix()

--- numac_porting/helpers.py
+++ numac_porting/helpers.py
@@ -2,12 +2,15 @@ def clamp(x, minimum, maximum):
     return max(minimum, min(x, maximum))
 
 def speedPhaseFix(clocktime, loopLenOld, loopLen):
+    # clocktime: us
+    # loopLenOld, loopLen: ms
+    # Returns ms
     # NOTE: Return value can be negative
     #print(clocktime, loopLenOld, loopLen)
     #time_ms = clocktime/1000
-    return (((clocktime/1000) % loopLenOld) / loopLenOld -
-        ((clocktime/1000) % loopLen) / loopLen * loopLen)
-
+    return loopLen * \
+            (((clocktime/1000) % loopLenOld) / loopLenOld -
+        ((clocktime/1000) % loopLen) / loopLen)
 
 #def interpolate(val, a,b, c,d):
 #    pass # TODO
